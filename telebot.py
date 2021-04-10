00cc39818cb8df246fe76fb6d619ba8185db9eb4
kartexxx <81906852+kartexxx@users.noreply.github.com>
2021-04-10 23:04:46 +0300

    Update telebot.py

--- telebot.py
+++ telebot.py
@@ -5,7 +5,7 @@ from google_trans_new import LANGUAGES, google_translator
 translator = google_translator()
 lang = "en"
 
-bot = telebot.TeleBot("TOKEN", skip_pending=True)
+bot = telebot.TeleBot("1716068298:AAF_WQrVuYSVZUupw-T4GHZzhqIjQ_EhO5c", skip_pending=True)
 
 @bot.message_handler(content_types=["text"])
 def cevri(message):
